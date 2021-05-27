220096b735bdee3368d75462fc5f8adf7c8c7ca4
danya131100 <danya131100@gmail.com>
2021-05-27 17:46:39 +0300

    second

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -6,23 +6,40 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public abstract class Course
+    public class Course
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public string URL { get; set; }
         public bool IsPaid { get; set; }
+        public int Price { get; set; }
         public int Listeners { get; set; }
         public int Reviews { get; set; }
         public int Lectures { get; set; }
         public string Difficulty { get; set; }
         public double Duration { get; set; }
+        internal double LecturesPerWeek { get; set; }
+
+        public Course(int id, string name, string url, bool ispaid, int price, int listeners, int reviews, int lectures, string difficulty, double duration)
+        {
+            ID = id;
+            Name = name;
+            URL = url;
+            IsPaid = ispaid;
+            Price = price;
+            Listeners = listeners;
+            Reviews = reviews;
+            Lectures = lectures;
+            Difficulty = difficulty;
+            Duration = duration;
+            if (IsPaid == false)
+                LecturesPerWeek = Lectures / (Duration / 4.0);
+        }
 
         public override string ToString()
         {
             string course = ID + " " + Name;
             return course;
         }
-
     }
 }
--- ClassLibrary/CourseList.cs
+++ ClassLibrary/CourseList.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class CourseList
+    internal class CourseList
     {
         public List<Course> Courses { get; set; }
         public int Count { get; set; }
@@ -23,9 +24,34 @@ namespace ClassLibrary
             Count++;
         }
 
-        public void NoLongerThanThreeMonths
+        public void NoLongerThanThreeMonths()
         {
+            Console.WriteLine("Платные курсов длительностью не более 3 месяцев:");
+            var selectedCourses = from c in Courses where (c.Duration < 3 && c.IsPaid) select c;
+            foreach (var c in selectedCourses)
+                Console.WriteLine(c);
+            Console.WriteLine("Бесплатные курсов длительностью не более 3 месяцев:");
+            selectedCourses = from c in Courses where (c.Duration < 3 && !c.IsPaid) select c;
+            foreach (var c in selectedCourses)
+                Console.WriteLine(c);
+        }
 
+        public void TenMostListenedSortedByPrice()
+        {
+            var selectedCourses = (from c in Courses where (c.IsPaid == true) orderby c.Listeners descending select c).Take(10).OrderBy(c => c.Price);
+            using (var SW = new StreamWriter("output.txt"))
+            {
+                foreach (var c in selectedCourses)
+                    Console.WriteLine(c);
+            }
+        }
+
+        public void FreeCoursesByLecturePerWeek()
+        {
+            var selectedCourses = (from c in Courses where (c.IsPaid == false) orderby c.LecturesPerWeek select c);
+            Console.WriteLine("Бесплатные курсы отсортированные по среднему количеству лекций в неделю:");
+            foreach (var c in selectedCourses)
+                Console.WriteLine(c + " - " + c.LecturesPerWeek);
         }
     }
 }
