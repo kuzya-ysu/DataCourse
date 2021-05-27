d8db1b8bfc56b3b9b143a321b8b6fad7fe76e179
kuzmin1 <kuzmin1@PC403-24>
2021-05-27 11:47:06 +0300

    Add project files.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public abstract class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public bool IsPaid { get; set; }
+        public int Listeners { get; set; }
+        public int Reviews { get; set; }
+        public int Lectures { get; set; }
+        public string Difficulty { get; set; }
+        public double Duration { get; set; }
+
+        public override string ToString()
+        {
+            string course = ID + " " + Name;
+            return course;
+        }
+
+    }
+}
---
+++ ClassLibrary/CourseList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class CourseList
+    {
+        public List<Course> Courses { get; set; }
+        public int Count { get; set; }
+
+        public CourseList()
+        {
+            Courses = new List<Course>();
+            Count = 0;
+        }
+
+        public void Add(Course course)
+        {
+            Courses.Add(course);
+            Count++;
+        }
+
+        public void NoLongerThanThreeMonths
+        {
+
+        }
+    }
+}
